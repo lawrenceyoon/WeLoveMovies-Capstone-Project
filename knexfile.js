74720411e6f13ff3b4461c52139b76f9f403e6e1
Lawrence Yoon <yoonlawrence@yahoo.com>
2021-06-18 13:01:54 -0700

    need to add one more table

--- knexfile.js
+++ knexfile.js
@@ -1,46 +1,47 @@
-const path = require("path");
+const path = require('path');
 
-if (process.env.USER) require("dotenv").config();
+if (process.env.USER) require('dotenv').config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  // DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = 'postgres://ribanbxe:loK5ic5r9f-hs66grOZdy0x1lqdFRtLU@kashin.db.elephantsql.com/ribanbxe',
 } = process.env;
 
 module.exports = {
   development: {
-    client: "postgresql",
+    client: 'postgresql',
     connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds'),
     },
   },
 
   production: {
-    client: "postgresql",
+    client: 'postgresql',
     connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds'),
     },
   },
 
   test: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: ":memory:",
+      filename: ':memory:',
     },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds'),
     },
     useNullAsDefault: true,
   },
--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
-if (process.env.USER) require("dotenv").config();
-const express = require("express");
+if (process.env.USER) require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Not found handler
+app.use((req, res, next) => {
+  next({ status: 404, message: `Not found: ${req.originalUrl}` });
+});
+
+// Error handler
+app.use((error, req, res, next) => {
+  console.error(error);
+  const { status = 500, message = 'Something went wrong!' } = error;
+  res.status(status).json({ error: message });
+});
 
 module.exports = app;
